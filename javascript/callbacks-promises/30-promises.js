6333a2da7025bc17773b3a8ab7755dd707951cdf
Michael Bamford <michael@michaelbamford.com>
2018-05-21 18:27:17 +1000

    Update promises with then use cases

--- javascript/callbacks-promises/30-promises.js
+++ javascript/callbacks-promises/30-promises.js
@@ -1,33 +1,49 @@
 (function() {
     new Promise((resolve, reject) => {
-        setTimeout(() => { 
+        setTimeout(() => {
             const result = Math.random()
-            if (result > 0) {
-                console.log(result)
+            if (result > 0.5) {
+                console.log(`initial=${result}`)
                 resolve(result)
             } else {
                 reject("You fool! You've done something wrong!")
             }
         }, 1500)
     }).then((value) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                value = value * 2
-                console.log(`resolved to ${value}`)
-                resolve(value)
-            }, 1500)
-        })
-    }).then((value) => { 
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                value = value * 10
-                console.log(`resolved to ${value}`)
-                resolve(value)
-            }, 1500)
-        })
-    }).then((finalValue) => {
-        console.log(`final value=${finalValue}`)
-    }).catch(
-        (message) => { console.log(message) }
-    )
+      // Return a value; the promise returned by `then` gets resolved with
+      // the returned value as its value.
+      return value * 2
+    }).then((value) => {
+      // Return another pending promise object; the resolution/rejection of
+      // the promise returned by then will be subsequent to the
+      // resolution/rejection of the promise returned by the handler.
+      // Also, the value of the promise returned by then will be the same as
+      // the value of the promise returned by the handler.
+      return new Promise((resolve, reject) => {
+          // Pretend that the call to multiple a number by 10 is an
+          // asynchronous operation and takes 1.5 seconds to compute.
+          setTimeout(() => {
+              resolve(value * 10)
+          }, 1500)
+      })
+    }).then((value) => {
+        // Return a hard-coded resolved promise.
+        return Promise.resolve(value * 10)
+    }).then((value) => {
+        // Throw an error; the promise returned by `then` gets rejected with
+        // the thrown error as its value;
+        throw new Error(value)
+    }).then((value) => {
+      console.log("You'll never see this line printed in the console")
+    }).catch((error) => {
+      // Deal with the errors then return the value to continue the chain;
+      // `catch` itself returns a resolved Promise. Note that this `catch`
+      // catches both the `throw` immediately above as well as the reject
+      // in the initial promise.
+      console.log(error)
+      return error.message
+    }).then((value) => {
+      console.log(`result=${value}`)
+    })
 })()
+
