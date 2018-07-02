bc5d87aca9c786af4ec8b08dda132feb839e3cb3
Michael Bamford <michael@michaelbamford.com>
2018-07-02 12:22:53 +1000

    Add dotenv to movies/api

--- javascript/movies/api/models/db.js
+++ javascript/movies/api/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-mongoose.connect('mongodb://localhost/movies')
+mongoose.connect(process.env.MONGODB_URL)
 
 module.exports = { mongoose, Schema }
